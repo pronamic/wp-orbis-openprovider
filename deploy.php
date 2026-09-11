231debc122b4a6e9422c62881d12a595e1dcb90a
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2026-09-11 09:56:38 +0200

    Update production deploy path
    Adjust the Deployer configuration for `orbis.pronamic.nl` to use the current remote project directory name. This keeps deployments targeting the correct path on the server.

--- deploy.php
+++ deploy.php
@@ -19,7 +19,7 @@ host( 'orbis.pronamic.nl' )
 	->set( 'hostname', 'esm7.siteground.biz' )
 	->set( 'remote_user', 'u155-jlog1cramrrx' )
 	->set( 'port', 18765 )
-	->set( 'deploy_path', '~/projects/wp-orbis-openprovider' )
+	->set( 'deploy_path', '~/projects/orbis-openprovider' )
 	->set( 'plugins_dir', '~/www/orbis.pronamic.nl/public_html/wp-content/plugins' );
 
 /**
